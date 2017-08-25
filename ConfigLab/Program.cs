0332ea9261340efc41c3e01a2e5740d11610d427
Carl Hjerpe <carl.hjerpe@dialect.se>
2017-08-25 14:29:41 +0200

    Initial commit

--- ConfigLab/Program.cs
+++ ConfigLab/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using JsonConfig;
+
+using Conf1 = JsonConfig.ConfigMgr<ConfigLab.Config>;
+
 namespace ConfigLab
 {
-    class Program
+    public class Config
     {
-        static void Main(string[] args)
+        public string string1 = "";
+        public int int1 = 0;
+        public double double1 = 0;
+        public Config_Ape ape1 = new Config_Ape();
+    }
+
+    public class Config_Ape
+    {
+        public string string2 = "";
+        public int int2 = 0;
+        public double double2 = 0;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
         {
+            Conf1.Load("config.json");
+            Conf1.Save();
+
+            Console.ReadKey();
         }
     }
 }
